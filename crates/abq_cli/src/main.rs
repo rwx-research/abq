3792cd8521562f0ea106737e5b78b9d2207c021c
Ayaz <20735482+ayazhafiz@users.noreply.github.com>
2022-07-11 15:49:04 -0500

    Have queue invocation specify the runner a worker should use (#40)
    This resolves the restriction in #35 where we required the user to
    specify what runner a worker should use, rather than the worker getting
    that info from the queue.

--- crates/abq_cli/src/main.rs
+++ crates/abq_cli/src/main.rs
@@ -4,6 +4,7 @@ mod workers;
 
 use abq_output::format_result;
 use abq_queue::invoke::invoke_work;
+use abq_utils::net_protocol::workers::{NativeTestRunnerParams, RunnerKind};
 use clap::Parser;
 
 use args::{CargoCmd, Cli, Command};
@@ -16,10 +17,9 @@ fn main() {
     match args.command {
         Command::Start { bind } => instance::start_abq_forever(bind),
         Command::Work {
-            kind,
             working_dir,
             queue_addr,
-        } => workers::start_workers(kind, working_dir, queue_addr),
+        } => workers::start_workers(working_dir, queue_addr),
         Command::Echo { strings: _ } => {
             todo!();
             // let abq = instance::find_abq();
@@ -37,7 +37,12 @@ fn main() {
             let on_result = |_, test_result| {
                 println!("{}", format_result(test_result));
             };
-            invoke_work(abq.server_addr(), on_result);
+            let jest_runner = RunnerKind::GenericNativeTestRunner(NativeTestRunnerParams {
+                cmd: "yarn".to_string(),
+                args: vec!["jest".to_string()],
+                extra_env: Default::default(),
+            });
+            invoke_work(abq.server_addr(), jest_runner, on_result);
         }
     }
 }
