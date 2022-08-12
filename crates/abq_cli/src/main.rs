d5e799921143cc989fb67155945891105da88895
Ayaz <20735482+ayazhafiz@users.noreply.github.com>
2022-08-11 22:11:28 -0700

    Add file logging (#84)

--- crates/abq_cli/src/main.rs
+++ crates/abq_cli/src/main.rs
@@ -14,19 +14,52 @@ use args::{default_num_workers, Cli, Command};
 
 use instance::AbqInstance;
 use reporting::{ExitCode, ReporterKind, SuiteReporters};
-use tracing_subscriber::{fmt, EnvFilter};
+use tracing_subscriber::{fmt, layer, prelude::*, EnvFilter, Registry};
 
 fn main() -> anyhow::Result<()> {
     let exit_code = abq_main()?;
     std::process::exit(exit_code.get());
 }
 
-fn abq_main() -> anyhow::Result<ExitCode> {
-    let tracing_fmt = fmt()
+struct TracingGuards {
+    _file_appender_guard: Option<tracing_appender::non_blocking::WorkerGuard>,
+}
+
+#[must_use]
+fn setup_tracing() -> TracingGuards {
+    // Trace to standard error with ABQ_LOG set
+    let stderr_layer = fmt::Layer::default()
+        .with_writer(std::io::stderr)
         .with_span_events(fmt::format::FmtSpan::ACTIVE)
-        .with_env_filter(EnvFilter::from_env("ABQ_LOG"))
-        .finish();
-    tracing::subscriber::set_global_default(tracing_fmt).unwrap();
+        .with_filter(EnvFilter::from_env("ABQ_LOG"));
+
+    // Trace to a rotating log file under ABQ_FILE_LOG_DIR, if it's set.
+    let (file_log_layer, file_appender_guard) = if let Ok(dir) = std::env::var("ABQ_FILE_LOG_DIR") {
+        let file_appender = tracing_appender::rolling::hourly(dir, "abq.log");
+        let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+        (
+            fmt::Layer::default()
+                .with_writer(non_blocking)
+                .with_ansi(false)
+                .boxed(),
+            Some(guard),
+        )
+    } else {
+        (layer::Identity::new().boxed(), None)
+    };
+
+    Registry::default()
+        .with(stderr_layer)
+        .with(file_log_layer)
+        .init();
+
+    TracingGuards {
+        _file_appender_guard: file_appender_guard,
+    }
+}
+
+fn abq_main() -> anyhow::Result<ExitCode> {
+    let _tracing_guards = setup_tracing();
 
     let Cli { command } = Cli::parse();
 
