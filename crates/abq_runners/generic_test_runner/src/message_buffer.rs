eb095d9d629ce625144c62dcdb188e405368b1f6
Ayaz <ayaz@rwx.com>
2022-11-18 11:04:44 -0600

    Implement concurrent test fetching/test running on the worker side
    Original PR: 291
    Original Commit: 3eaf8d0f624297b834614f53b79f2e249a201876
    
    Co-authored-by: Michael Glass <me@mike.is>

--- crates/abq_runners/generic_test_runner/src/message_buffer.rs
+++ crates/abq_runners/generic_test_runner/src/message_buffer.rs
@@ -25,14 +25,21 @@ impl<T> std::fmt::Debug for Msg<T> {
 pub(crate) struct BatchedProducer<T> {
     msg_tx: mpsc::Sender<Msg<T>>,
     refill_rx: mpsc::Receiver<()>,
-    exit_rx: oneshot::Receiver<()>,
 }
 
 pub(crate) struct BatchedConsumer<T> {
     msg_rx: mpsc::Receiver<Msg<T>>,
     refill_tx: mpsc::Sender<()>,
-    exit_tx: oneshot::Sender<()>,
     refill_strategy: RefillStrategy,
+
+    /// Whether the consumer should attempt a refill if the refill strategy is met.
+    /// This is true between the time a new batch is received and the refill strategy is met, and
+    /// false for all times after the refill strategy is met but before the current batch is
+    /// exhausted.
+    eligible_for_refill: bool,
+
+    current_batch_size: usize,
+    current_batch_processed: usize,
 }
 
 /// When a producer should refill the batch buffer of messages.
@@ -43,7 +50,7 @@ pub(crate) enum RefillStrategy {
 }
 
 /// Create (producer, consumer) channels for processing batched messages.
-fn channel<T>(
+pub(crate) fn channel<T>(
     capacity: usize,
     refill_strategy: RefillStrategy,
 ) -> (BatchedProducer<T>, BatchedConsumer<T>) {
@@ -51,57 +58,28 @@ fn channel<T>(
     // refill the channel.
     let (msg_tx, msg_rx) = mpsc::channel(capacity * 2);
     let (refill_tx, refill_rx) = mpsc::channel(1);
-    let (exit_tx, exit_rx) = oneshot::channel();
 
-    let producer = BatchedProducer {
-        msg_tx,
-        refill_rx,
-        exit_rx,
-    };
+    let producer = BatchedProducer { msg_tx, refill_rx };
     let consumer = BatchedConsumer {
         msg_rx,
         refill_tx,
-        exit_tx,
         refill_strategy,
+
+        eligible_for_refill: true,
+
+        current_batch_size: 0,
+        current_batch_processed: 0,
     };
 
     (producer, consumer)
 }
 
-/// Creates producer and consumer channels for processing batched messages, returning a future that
-/// resolves when both are complete.
-///
-/// The future completes when
-/// - the corresponding [MsgProducer] closes the channel
-/// - `process_msg` returns an error, which will stop the producer, and return all pending
-///   messages in the channel.
-pub(crate) async fn run_channels<T, E, P, PFut, I, Iter, C, CFut>(
-    capacity: usize,
-    refill_strategy: RefillStrategy,
-    fetch_msgs: P,
-    process_msg: C,
-) -> Result<(), (E, Vec<T>)>
-where
-    P: Fn() -> PFut,
-    PFut: IntoFuture<Output = (I, Completed)>,
-    I: IntoIterator<IntoIter = Iter>,
-    Iter: ExactSizeIterator<Item = T>,
-
-    C: Fn(T) -> CFut,
-    CFut: IntoFuture<Output = Result<(), E>>,
-{
-    let (tx, rx) = channel(capacity, refill_strategy);
-    let tx = tx.start(fetch_msgs);
-    let rx = rx.start(process_msg);
-    let ((), rx_result) = tokio::join!(tx, rx);
-    rx_result
-}
-
-pub(crate) struct Completed(bool);
+pub(crate) struct Completed(pub bool);
 
 impl<T> BatchedProducer<T> {
     /// Returns a future that runs the producer, with a task to fetch messages.
-    /// The future completes only when the consumer has indicated it has completed as well.
+    /// The future completes when the generator indicates that it is complete, or the consumer
+    /// exits.
     pub async fn start<F, Fut, I, Iter>(mut self, fetch_msgs: F)
     where
         F: Fn() -> Fut,
@@ -115,87 +93,102 @@ impl<T> BatchedProducer<T> {
             (msgs, completed) = fetch_msgs().await;
 
             let msgs = msgs.into_iter();
-            self.msg_tx
-                .send(Msg::BatchSize(msgs.len()))
-                .await
-                .expect("internal error - message channel closed early by consumer");
+
+            // If we fail to send the message, the consumer has exited early and we have nothing
+            // more to do.
+            if self.msg_tx.send(Msg::BatchSize(msgs.len())).await.is_err() {
+                return;
+            };
             for msg in msgs {
-                self.msg_tx
-                    .send(Msg::Msg(msg))
-                    .await
-                    .expect("internal error - message channel closed early by consumer");
+                if self.msg_tx.send(Msg::Msg(msg)).await.is_err() {
+                    return;
+                }
             }
 
             if completed.0 {
                 return;
             }
 
-            tokio::select! {
-                _ = self.refill_rx.recv() => {
-                    continue;
-                }
-                _ = &mut self.exit_rx => {
-                    return;
-                }
+            if self.refill_rx.recv().await.is_none() {
+                // Consumer has exited
+                return;
             }
         }
     }
 }
 
 impl<T> BatchedConsumer<T> {
-    /// Returns a future that runs the consumer, with a task to process messages.
-    /// The future completes only when
-    /// - the corresponding [MsgProducer] closes the channel
-    /// - `process_msg` returns an error, which will stop the producer, and return all pending
-    ///   messages in the channel.
-    pub async fn start<F, Fut, E>(mut self, process_msg: F) -> Result<(), (E, Vec<T>)>
-    where
-        F: Fn(T) -> Fut,
-        Fut: IntoFuture<Output = Result<(), E>>,
-    {
-        let mut refill_at = 0;
-        let mut processed = 0;
-        loop {
-            let msg = match self.msg_rx.recv().await {
-                Some(msg) => msg,
-                None => return Ok(()), // all done
-            };
+    /// Returns the next message in the channel, or [None] if the channel is complete.
+    pub async fn recv(&mut self) -> Option<T> {
+        // If the channel is closed, we're all done.
+        let msg = self.msg_rx.recv().await?;
 
-            let msg = match msg {
-                Msg::BatchSize(n) => {
-                    match self.refill_strategy {
-                        RefillStrategy::HalfConsumed => {
-                            refill_at = n / 2;
-                        }
-                    }
-                    continue;
+        let msg = match msg {
+            Msg::BatchSize(size) => {
+                self.eligible_for_refill = true;
+                self.current_batch_size = size;
+                self.current_batch_processed = 0;
+
+                let msg = self.msg_rx.recv().await?;
+
+                match msg {
+                    Msg::BatchSize(_) => panic!("Unexpected batch size message"),
+                    Msg::Msg(msg) => msg,
                 }
-                Msg::Msg(msg) => msg,
-            };
-
-            processed += 1;
-
-            if processed >= refill_at {
-                processed = 0;
-                let refill_tx = self.refill_tx.clone();
-                tokio::spawn(async move { refill_tx.send(()).await });
             }
+            Msg::Msg(msg) => msg,
+        };
 
-            if let Err(err) = process_msg(msg).await {
-                self.exit_tx
-                    .send(())
-                    .expect("internal error - producer channel closed unexpectedly");
+        self.current_batch_processed += 1;
 
-                let mut remaining = vec![];
-                while let Some(msg) = self.msg_rx.recv().await {
-                    match msg {
-                        Msg::BatchSize(_) => continue,
-                        Msg::Msg(msg) => remaining.push(msg),
-                    }
+        let should_refill = self.eligible_for_refill
+            && match self.refill_strategy {
+                RefillStrategy::HalfConsumed => {
+                    self.current_batch_processed >= self.current_batch_size / 2
                 }
-                return Err((err, remaining));
+            };
+
+        if should_refill {
+            // At this point the refill channel should always be empty, because we have not yet
+            // requested a refill and the channel has a capacity of 1 message. As such, fire
+            // off the request in-path, as this call is likely to be very cheap and will wake
+            // the refill task without the additional cost(s) of a background task.
+            //
+            // If the message fails to be sent, the producer has already reached the end of the
+            // producing stream and has exited, leaving us nothing more to do.
+            let _err = self.refill_tx.send(()).await;
+
+            self.eligible_for_refill = false;
+        }
+
+        Some(msg)
+    }
+
+    /// Shuts down the consumer and producer, returning all pending messages from the producer. The
+    /// producer will not asked to refill the message channel.
+    pub async fn flush(self) -> Vec<T> {
+        let Self {
+            mut msg_rx,
+            refill_tx,
+
+            refill_strategy: _,
+            eligible_for_refill: _,
+            current_batch_size: _,
+            current_batch_processed: _,
+        } = self;
+
+        // Close the refill channel, so that the producer knows that we are not
+        // waiting any more and can exit.
+        drop(refill_tx);
+
+        let mut remaining = vec![];
+        while let Some(msg) = msg_rx.recv().await {
+            match msg {
+                Msg::BatchSize(_) => continue,
+                Msg::Msg(msg) => remaining.push(msg),
             }
         }
+        remaining
     }
 }
 
@@ -203,12 +196,46 @@ impl<T> BatchedConsumer<T> {
 mod test {
     use std::{
         convert::Infallible,
+        future::IntoFuture,
         sync::{atomic::AtomicU8, Arc},
     };
 
     use abq_utils::atomic;
 
-    use super::{channel, run_channels, Completed, RefillStrategy};
+    use super::{channel, Completed, RefillStrategy};
+
+    async fn run_channels<T, E, P, PFut, I, Iter, C, CFut>(
+        capacity: usize,
+        refill_strategy: RefillStrategy,
+        fetch_msgs: P,
+        process_msg: C,
+    ) -> Result<(), (E, Vec<T>)>
+    where
+        P: Fn() -> PFut,
+        PFut: IntoFuture<Output = (I, Completed)>,
+        I: IntoIterator<IntoIter = Iter>,
+        Iter: ExactSizeIterator<Item = T>,
+
+        C: Fn(T) -> CFut,
+        CFut: IntoFuture<Output = Result<(), E>>,
+    {
+        let (tx, mut rx) = channel(capacity, refill_strategy);
+        let tx = tx.start(fetch_msgs);
+
+        let run_rx_to_completion = async {
+            while let Some(msg) = rx.recv().await {
+                if let Err(e) = process_msg(msg).await {
+                    let remaining = rx.flush().await;
+                    return Err((e, remaining));
+                }
+            }
+
+            Ok(())
+        };
+
+        let ((), rx_result) = tokio::join!(tx, run_rx_to_completion);
+        rx_result
+    }
 
     #[tokio::test]
     async fn refill_with_reload_one() {
@@ -286,5 +313,35 @@ mod test {
         assert_eq!(rem_msgs, &[2, 3, 4]);
 
         assert_eq!(fetch_count.load(atomic::ORDERING), 1);
+    }
+
+    #[tokio::test]
+    async fn run_channels_with_consumer_early_exit() {
+        let fetch_count = AtomicU8::new(0);
+        let fetch_msgs = || async {
+            let n = fetch_count.fetch_add(1, atomic::ORDERING);
+            match n {
+                0 => (vec![1], Completed(false)),
+                1 => (vec![2], Completed(false)),
+                _ => unreachable!(),
+            }
+        };
+
+        let (tx, mut rx) = channel(2, RefillStrategy::HalfConsumed);
+        let tx = tx.start(fetch_msgs);
+
+        let run_rx = async {
+            // Pull one message and immediately exit - this should force a refill on the producer
+            // side, but the consumer may not be around.
+            let msg = rx.recv().await.unwrap();
+            assert_eq!(msg, 1);
+
+            drop(rx);
+        };
+
+        let ((), ()) = tokio::join!(tx, run_rx);
+
+        // Initial fetch + fetch after first pull
+        assert_eq!(fetch_count.load(atomic::ORDERING), 2);
     }
 }
