f366018b3024eece024ee7ea3c73b39719ef0e2d
Ayaz <20735482+ayazhafiz@users.noreply.github.com>
2023-01-12 08:11:55 -0600

    Add a check for the total number of run tests during benchmarks (#415)
    * Add a check for the total number of run tests during benchmarks
    
    * Fix assertion
    
    * Make sure abq and rspec agree

--- bigtest/benchmark/test.sh
+++ bigtest/benchmark/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim: foldmethod=marker foldmarker={{{,}}}
 
 set -eu
 
@@ -31,6 +32,22 @@ abq test \
   | tee $ABQ_BENCHMARK_OUT
 
 bundle exec rspec bigtest/benchmark/benchmark_spec.rb | tee $RSPEC_BENCHMARK_OUT
+
+# {{{ assert_num_tests
+re_abq_test_summary_line="[0-9]+ tests, [0-9]+ failures"
+re_rspec_test_summary_line="[0-9]+ examples, [0-9]+ failures"
+expected_num_tests=500
+actual_abq_num_tests="$(cat "$ABQ_BENCHMARK_OUT" | grep -Eo "$re_abq_test_summary_line" | grep -Eo "^[0-9]+")"
+actual_rspec_num_tests="$(cat "$RSPEC_BENCHMARK_OUT" | grep -Eo "$re_rspec_test_summary_line" | grep -Eo "^[0-9]+")"
+if [ "$expected_num_tests" -ne "$actual_abq_num_tests" ]; then
+  echo "Expected $expected_num_tests tests run; ABQ recorded running $actual_abq_num_tests"
+  exit 1
+fi
+if [ "$expected_num_tests" -ne "$actual_rspec_num_tests" ]; then
+  echo "Expected $expected_num_tests tests run; RSpec recorded running $actual_rspec_num_tests"
+  exit 1
+fi
+# }}} assert_num_tests
 
 RE_FLOAT="[0-9]+(\.[0-9]+)?"
 RE_WORKER_SUMMARY_LINE="Finished in $RE_FLOAT seconds \(files"
