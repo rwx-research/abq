880004774fb739ab5b76c61e5c53f48bb1db895d
Ayaz <20735482+ayazhafiz@users.noreply.github.com>
2023-02-01 10:34:26 -0600

    Add timeouts for setting out-of-band exit codes (#456)
    * Add a reason to timeouts
    
    As we look forward to adding timeouts for when an OOB exit code is
    expected, it will be useful for us to keep around a reason for why each
    timeout is fired.
    
    * Rename timeout resolution method
    
    * Handle fired timeout for OOB exit code timeouts
    
    * Start inserting timeouts for OOB exit codes
    
    * Improve timeout resolution API
    
    * Fix tests for timeout
    
    * Update interface for creating timeout strategy
    
    * Add test for timing out receipt of OOB exit code
    
    * Add a cancellation reason to OOB exit code setting messages
    
    * Lint
    
    * Fix tests
    
    * rename LastResultNotReceived to ResultNotReceived
    
    ---------
    
    Co-authored-by: Michael Glass <me@mike.is>

--- crates/abq_queue/src/timeout.rs
+++ crates/abq_queue/src/timeout.rs
@@ -5,45 +5,107 @@ use std::{
     time::Duration,
 };
 
-use abq_utils::net_protocol::workers::RunId;
+use abq_utils::net_protocol::{queue::CancelReason, workers::RunId};
 use futures::{stream::FuturesUnordered, StreamExt};
 use std::future::Future;
 use tokio::sync::RwLock;
 
 #[derive(Debug, Clone, Copy)]
-pub enum RunTimeoutStrategy {
+pub struct RunTimeoutStrategy(RunTimeoutStrategyPriv);
+
+impl RunTimeoutStrategy {
     /// Determine the timeout from the parameters of a test run.
-    RunBased,
+    pub const RUN_BASED: Self = RunTimeoutStrategy(RunTimeoutStrategyPriv::RunBased);
+
     /// Constant timeout, for use in testing.
-    Constant(Duration),
+    pub fn constant(f: fn(TimeoutReason) -> Duration) -> Self {
+        Self(RunTimeoutStrategyPriv::Constant(f))
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum RunTimeoutStrategyPriv {
+    RunBased,
+    Constant(fn(TimeoutReason) -> Duration),
 }
 
 impl Default for RunTimeoutStrategy {
     fn default() -> Self {
-        Self::RunBased
+        Self::RUN_BASED
     }
 }
 
+/// The maximum time reserved to wait for an out-of-band exit code for a test run.
+const OOB_EXIT_CODE_TIMEOUT: Duration = Duration::from_secs(3 * 60); // 3 minutes
+
 impl RunTimeoutStrategy {
-    pub fn resolve(&self, timeout_for_run: Duration) -> Duration {
-        match self {
-            RunTimeoutStrategy::RunBased => timeout_for_run,
-            RunTimeoutStrategy::Constant(timeout) => *timeout,
-        }
+    /// Determine a timeout duration for [TimeoutReason::ResultNotReceived].
+    pub(crate) fn timeout_for_last_test_result(&self, timeout_for_run: Duration) -> TimeoutSpec {
+        let reason = TimeoutReason::ResultNotReceived;
+        let duration = match self.0 {
+            RunTimeoutStrategyPriv::RunBased => timeout_for_run,
+            RunTimeoutStrategyPriv::Constant(timeout) => timeout(reason),
+        };
+        TimeoutSpec { duration, reason }
+    }
+
+    /// Determine a timeout duration for [TimeoutReason::OOBExitCodeNotReceived].
+    pub(crate) fn timeout_for_oob_exit_code(&self) -> TimeoutSpec {
+        let reason = TimeoutReason::OOBExitCodeNotReceived;
+        let duration = match self.0 {
+            RunTimeoutStrategyPriv::RunBased => {
+                // The OOB exit code timeout is constant for all runs.
+                OOB_EXIT_CODE_TIMEOUT
+            }
+            RunTimeoutStrategyPriv::Constant(timeout) => timeout(reason),
+        };
+        TimeoutSpec { duration, reason }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub(crate) struct TimeoutSpec {
+    duration: Duration,
+    reason: TimeoutReason,
+}
+
+impl TimeoutSpec {
+    pub fn duration(&self) -> Duration {
+        self.duration
     }
 }
 
 /// Records runs that should timeout after a certain amount of time.
 /// Fires [RunTimeoutManager::next_timeout] when a timeout is reached.
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub(crate) struct RunTimeoutManager {
+    strategy: RunTimeoutStrategy,
     timeouts: Arc<RwLock<FuturesUnordered<TimeoutCell>>>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum TimeoutReason {
+    /// A test run was timed-out because the last test result for a run was not received in time.
+    ResultNotReceived,
+    /// A test run was timed-out because it was marked to have an OOB exit code set, and that exit
+    /// code was not received in time.
+    OOBExitCodeNotReceived,
+}
+
+impl From<TimeoutReason> for CancelReason {
+    fn from(reason: TimeoutReason) -> Self {
+        match reason {
+            TimeoutReason::ResultNotReceived => CancelReason::TimeoutOnLastTestResult,
+            TimeoutReason::OOBExitCodeNotReceived => CancelReason::TimeoutOnOutOfBandExitCode,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub(crate) struct TimedOutRun {
     pub run_id: RunId,
     pub after: Duration,
+    pub reason: TimeoutReason,
 }
 
 struct TimeoutCell {
@@ -61,16 +123,29 @@ impl Future for TimeoutCell {
 impl RunTimeoutManager {
     const MAX_TIMEOUT_WAIT_TIME: Duration = Duration::from_micros(10);
 
+    pub fn new(strategy: RunTimeoutStrategy) -> Self {
+        Self {
+            strategy,
+            timeouts: Default::default(),
+        }
+    }
+
+    pub fn strategy(&self) -> RunTimeoutStrategy {
+        self.strategy
+    }
+
     /// Inserts a new timeout for a given run ID.
     /// Thread safe and takes only a read over a read/write lock; this makes insertions relatively
     /// cheap across threads, while waiting for timeouts requires exclusive access.
-    pub async fn insert_run(&self, run_id: RunId, timeout: Duration) {
+    pub async fn insert_run(&self, run_id: RunId, timeout_spec: TimeoutSpec) {
+        let TimeoutSpec { duration, reason } = timeout_spec;
         let runs = self.timeouts.read().await;
         let fut = Box::pin(async move {
-            tokio::time::sleep(timeout).await;
+            tokio::time::sleep(duration).await;
             TimedOutRun {
                 run_id,
-                after: timeout,
+                after: duration,
+                reason,
             }
         });
         let timeout_cell = TimeoutCell { timeout: fut };
@@ -112,43 +187,59 @@ mod test {
 
     use abq_utils::net_protocol::workers::RunId;
 
-    use crate::timeout::TimedOutRun;
+    use crate::timeout::{RunTimeoutStrategy, TimedOutRun, TimeoutReason, TimeoutSpec};
 
     use super::RunTimeoutManager;
 
+    fn zero(_: TimeoutReason) -> Duration {
+        Duration::ZERO
+    }
+
     #[tokio::test]
     async fn fire_one_timeout() {
-        let manager = RunTimeoutManager::default();
+        let manager = RunTimeoutManager::new(RunTimeoutStrategy::constant(zero));
 
         let run_id = RunId::unique();
 
-        manager.insert_run(run_id.clone(), Duration::ZERO).await;
+        let spec = manager.strategy().timeout_for_oob_exit_code();
+
+        manager.insert_run(run_id.clone(), spec).await;
         assert_eq!(
             manager.next_timeout().await,
             TimedOutRun {
                 run_id,
-                after: Duration::ZERO
+                after: Duration::ZERO,
+                reason: TimeoutReason::OOBExitCodeNotReceived,
             }
         );
     }
 
     #[tokio::test]
     async fn fire_multiple_timeouts() {
-        let manager = RunTimeoutManager::default();
+        let manager = RunTimeoutManager::new(RunTimeoutStrategy::constant(zero));
 
         let run_id1 = RunId::unique();
         let run_id2 = RunId::unique();
         let run_id3 = RunId::unique();
 
-        manager.insert_run(run_id1.clone(), Duration::ZERO).await;
-        manager.insert_run(run_id2.clone(), Duration::ZERO).await;
-        manager.insert_run(run_id3.clone(), Duration::ZERO).await;
+        let spec = TimeoutSpec {
+            duration: Duration::ZERO,
+            reason: TimeoutReason::ResultNotReceived,
+        };
+
+        manager.insert_run(run_id1.clone(), spec).await;
+        manager.insert_run(run_id2.clone(), spec).await;
+        manager.insert_run(run_id3.clone(), spec).await;
 
         let mut timed_out = HashSet::new();
         let mut afters = HashSet::new();
 
         for _ in 0..3 {
-            let TimedOutRun { run_id, after } = manager.next_timeout().await;
+            let TimedOutRun {
+                run_id,
+                after,
+                reason: _,
+            } = manager.next_timeout().await;
             timed_out.insert(run_id);
             afters.insert(after);
         }
@@ -164,7 +255,7 @@ mod test {
 
     #[tokio::test]
     async fn yield_to_contest() {
-        let manager = RunTimeoutManager::default();
+        let manager = RunTimeoutManager::new(RunTimeoutStrategy::default());
 
         // Make sure long-lived timeouts allow contest - `old` timeout here should allow `young` to
         // compete, and moreover `young` should always finish first.
@@ -172,13 +263,23 @@ mod test {
         let old_timeout = Duration::MAX;
         let old_run_id = RunId::unique();
 
+        let old_spec = TimeoutSpec {
+            duration: old_timeout,
+            reason: TimeoutReason::OOBExitCodeNotReceived,
+        };
+
         let young_timeout = Duration::from_micros(10);
         let young_run_id = RunId::unique();
 
-        manager.insert_run(old_run_id, old_timeout).await;
+        let young_spec = TimeoutSpec {
+            duration: young_timeout,
+            reason: TimeoutReason::OOBExitCodeNotReceived,
+        };
+
+        manager.insert_run(old_run_id, old_spec).await;
 
         tokio::select! {
-            _insert_young = manager.insert_run(young_run_id.clone(), young_timeout) => (),
+            _insert_young = manager.insert_run(young_run_id.clone(), young_spec) => (),
             _wait_for_old = manager.next_timeout() => unreachable!(),
         };
 
@@ -187,6 +288,7 @@ mod test {
             TimedOutRun {
                 run_id: young_run_id,
                 after: young_timeout,
+                reason: TimeoutReason::OOBExitCodeNotReceived
             }
         )
     }
