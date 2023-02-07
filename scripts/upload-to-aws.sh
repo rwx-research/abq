d34c62c58df9ef80dd4a716c199c60785551d5c2
Michael Glass <me@mike.is>
2023-02-07 17:23:04 +0100

    abq binary naming (#476)
    * rename `matrix.os` to `matrix.runs-on` to be more accurate
    
    * extract aws uploading to a script
    
    * rephrase comments
    
    * mark archival as deprecated
    
    * upload script uploads binary to rfc-20 style paths
    
    * avoid using matrix.platform
    
    * call platform deprecated
    
    * drop unused id
    
    * %s/RELEASE_NAME/RELEASE_DIR/
    
    * inline some unused shell vars
    
    * use upload to aws script from test_and_package_development
    
    * bigtest downloads from new path
    
    * drop unused env var

---
+++ scripts/upload-to-aws.sh
@@ -0,0 +1,43 @@
+#!/bin/env bash
+set -euo pipefail
+
+# set -u will fail this script if $RELEASE_ARCHIVE, $RELEASE_BUCKET, $RELEASE_S3_OBJECT, $RELEASE_S3_NIGHTLY_OBJECT, $RELEASE_DIR, $NEW_RELEASE_S3_OBJECT, or $NEW_RELEASE_S3_NIGHTLY_OBJECT are not set
+
+# this script uploads abq to s3
+#
+# It's used by the `build_and_upload` github workflow.
+
+ACCESS_KEY_ID=$1
+SECRET_ACCESS_KEY=$2
+PROFILE=$3
+
+aws configure set aws_access_key_id "$ACCESS_KEY_ID" --profile "$PROFILE"
+aws configure set aws_secret_access_key "$SECRET_ACCESS_KEY" --profile "$PROFILE"
+
+# deprecated format, remove after we're fully migrated to rfc-20 style binaries
+aws s3 cp "$RELEASE_ARCHIVE" "s3://$RELEASE_BUCKET/$RELEASE_S3_OBJECT" --profile staging
+aws s3api put-object-tagging \
+  --bucket "$RELEASE_BUCKET" \
+  --key "$RELEASE_S3_OBJECT" \
+  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')" \
+  --profile "$PROFILE"
+
+aws s3 cp \
+  "s3://$RELEASE_BUCKET/$RELEASE_S3_OBJECT" \
+  "s3://$RELEASE_BUCKET/$RELEASE_S3_NIGHTLY_OBJECT" \
+  --profile "$PROFILE"
+
+# rfc-20 style binaries
+aws s3 cp "$RELEASE_DIR/abq" "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT" --profile staging
+aws s3api put-object-tagging \
+  --bucket "$RELEASE_BUCKET" \
+  --key "$NEW_RELEASE_S3_OBJECT" \
+  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')" \
+  --profile "$PROFILE"
+
+aws s3 cp \
+  "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT" \
+  "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_NIGHTLY_OBJECT" \
+  --profile "$PROFILE"
+
+# TODO: also upload gzip and set content-encoding
