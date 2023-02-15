ced53768a1e89f793c2e86003a0c685bc9be1252
Ayaz <ayaz@rwx.com>
2023-02-15 05:14:26 -0600

    Attempt a profile for faster CI builds outside releases
    Original PR: 507
    Original Commit: 5f669df555efb4d50db0800a174871f0962cbe98
    
    Co-authored-by: Michael Glass <me@mike.is>

--- crates/abq_test_support/abq_test_utils/src/lib.rs
+++ crates/abq_test_support/abq_test_utils/src/lib.rs
@@ -5,7 +5,11 @@ pub const WORKSPACE: &str = env!("ABQ_WORKSPACE_DIR");
 pub fn artifacts_dir() -> PathBuf {
     let path = if cfg!(all(target_arch = "x86_64", target_env = "musl")) {
         // GHA is using a musl target
-        "target/x86_64-unknown-linux-musl/release"
+        if cfg!(debug_assertions) {
+            "target/x86_64-unknown-linux-musl/release-unstable"
+        } else {
+            "target/x86_64-unknown-linux-musl/release"
+        }
     } else if cfg!(debug_assertions) {
         "target/debug"
     } else {
