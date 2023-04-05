81c668d2b0a5fd15f51cc04f956220db02e2906a
Ayaz Hafiz <ayaz@rwx.com>
2023-04-05 09:23:48 -0500

    Update artifacts_dir

--- crates/abq_test_support/abq_test_utils/src/lib.rs
+++ crates/abq_test_support/abq_test_utils/src/lib.rs
@@ -27,12 +27,12 @@ pub mod color_writer;
 pub const WORKSPACE: &str = env!("ABQ_WORKSPACE_DIR");
 
 pub fn artifacts_dir() -> PathBuf {
-    let path = if cfg!(all(target_arch = "x86_64", target_env = "musl")) {
-        // GHA is using a musl target
+    let path = if cfg!(all(target_arch = "x86_64")) {
         if cfg!(debug_assertions) {
-            "target/x86_64-unknown-linux-musl/release-unstable"
+            // GHA x86 with debug assertions targets the release-unstable profile.
+            "target/release-unstable"
         } else {
-            "target/x86_64-unknown-linux-musl/release"
+            "target/release"
         }
     } else if cfg!(debug_assertions) {
         "target/debug"
