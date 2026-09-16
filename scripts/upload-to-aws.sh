25a74efeb3b20e9d48ff5a2054d05bf215e939bc
Jordan Sanders <jordan@rwx.com>
2026-09-16 14:15:39 -0500

    Switch to using OIDC to assume IAM roles

--- scripts/upload-to-aws.sh
+++ scripts/upload-to-aws.sh
@@ -7,50 +7,37 @@ set -euo pipefail
 #
 # It's used by the `build_and_upload` github workflow.
 
-ACCESS_KEY_ID=$1
-SECRET_ACCESS_KEY=$2
-PROFILE=$3
-
-aws configure set aws_access_key_id "$ACCESS_KEY_ID" --profile "$PROFILE"
-aws configure set aws_secret_access_key "$SECRET_ACCESS_KEY" --profile "$PROFILE"
-
 # deprecated format, remove after we're fully migrated to rfc-20 style binaries
-aws s3 cp "$RELEASE_ARCHIVE" "s3://$RELEASE_BUCKET/$RELEASE_S3_OBJECT" --profile "$PROFILE"
+aws s3 cp "$RELEASE_ARCHIVE" "s3://$RELEASE_BUCKET/$RELEASE_S3_OBJECT"
 aws s3api put-object-tagging \
   --bucket "$RELEASE_BUCKET" \
   --key "$RELEASE_S3_OBJECT" \
-  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')" \
-  --profile "$PROFILE"
+  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')"
 
 aws s3 cp \
   "s3://$RELEASE_BUCKET/$RELEASE_S3_OBJECT" \
-  "s3://$RELEASE_BUCKET/$RELEASE_S3_NIGHTLY_OBJECT" \
-  --profile "$PROFILE"
+  "s3://$RELEASE_BUCKET/$RELEASE_S3_NIGHTLY_OBJECT"
 
 # rfc-20 style binaries
-aws s3 cp "$RELEASE_DIR/abq" "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT" --profile "$PROFILE"
+aws s3 cp "$RELEASE_DIR/abq" "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT"
 aws s3api put-object-tagging \
   --bucket "$RELEASE_BUCKET" \
   --key "$NEW_RELEASE_S3_OBJECT" \
-  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')" \
-  --profile "$PROFILE"
+  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')"
 
 aws s3 cp \
   "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT" \
-  "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_NIGHTLY_OBJECT" \
-  --profile "$PROFILE"
+  "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_NIGHTLY_OBJECT"
 
 # rfc-20 style binaries for the tester harness
-aws s3 cp "$RELEASE_DIR/abq_tester_harness" "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT_TESTER_HARNESS" --profile "$PROFILE"
+aws s3 cp "$RELEASE_DIR/abq_tester_harness" "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT_TESTER_HARNESS"
 aws s3api put-object-tagging \
   --bucket "$RELEASE_BUCKET" \
   --key "$NEW_RELEASE_S3_OBJECT_TESTER_HARNESS" \
-  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')" \
-  --profile "$PROFILE"
+  --tagging "$(jq -n --arg commit "$(git rev-parse HEAD)" '{"TagSet": [{"Key":"commit", "Value": $commit}]}')"
 
 aws s3 cp \
   "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_OBJECT_TESTER_HARNESS" \
-  "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_NIGHTLY_OBJECT_TESTER_HARNESS" \
-  --profile "$PROFILE"
+  "s3://$RELEASE_BUCKET/$NEW_RELEASE_S3_NIGHTLY_OBJECT_TESTER_HARNESS"
 
 # TODO: also upload gzip and set content-encoding
